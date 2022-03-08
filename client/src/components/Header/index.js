61f73961a01e9a7b7609876e87852934f3b9d43a
Robert Xavier Alanis <59277298+robertalanis@users.noreply.github.com>
2022-03-08 12:02:02 -0600

    fixed bug where books would load before search was made

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -7,9 +7,10 @@ const Header = (props) => {
   return (
     <div>
       <Jumbotron>
-        <h1 className="display-4 text-center">{"(React) Google Books Search"}</h1>
+        <h1 className="display-4 text-center">{"Book Search"}</h1>
         <hr></hr>
         <h2 className="text-center">Search for and Save Books of Interest</h2>
+        <h5 className="text-center">Built with React and Powered by Google Books API</h5>
       </Jumbotron>
     </div>
   );
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -4,60 +4,71 @@ import Results from "../components/Results";
 import API from "../utils/API";
 
 class Search extends React.Component {
-    state = {
-        value: "",
-        books: []
-    };
+	state = {
+		value: "",
+		books: [],
+	};
 
-    componentDidMount() {
-        this.searchBook();
-    }
+	// componentDidMount() {
+	// 	this.searchBook();
+	// }
 
-    makeBook = bookData => {
-        return {
-            _id: bookData.id,
-            title: bookData.volumeInfo.title,
-            authors: bookData.volumeInfo.authors,
-            description: bookData.volumeInfo.description,
-            image: bookData.volumeInfo.imageLinks.thumbnail,
-            link: bookData.volumeInfo.previewLink
-        }
-    }
+	makeBook = (bookData) => {
+		return {
+			_id: bookData.id,
+			title: bookData.volumeInfo.title,
+			authors: bookData.volumeInfo.authors,
+			description: bookData.volumeInfo.description,
+			image: bookData.volumeInfo.imageLinks.thumbnail,
+			link: bookData.volumeInfo.previewLink,
+		};
+	};
 
-    searchBook = query => {
-        API.getBook(query)
-            .then(res => this.setState({ books: res.data.items.map(bookData => this.makeBook(bookData)) }))
-            .catch(err => console.error(err));
-    };
+	searchBook = (query) => {
+		API.getBook(query)
+			.then((res) =>
+				this.setState({ books: res.data.items.map((bookData) => this.makeBook(bookData)) })
+			)
+			.catch((err) => console.error(err));
+	};
 
-    handleInputChange = event => {
-        const name = event.target.name;
-        const value = event.target.value;
-        this.setState({
-            [name]: value
-        });
-    };
+	handleInputChange = (event) => {
+		const name = event.target.name;
+		const value = event.target.value;
+		this.setState({
+			[name]: value,
+		});
+	};
 
-    handleFormSubmit = event => {
-        event.preventDefault();
-        this.searchBook(this.state.search);
-    };
+	handleFormSubmit = (event) => {
+		event.preventDefault();
+		this.searchBook(this.state.search);
+	};
 
-    render() {
-        return (
-            <div>
-                <Form
-                    search={this.state.search}
-                    handleInputChange={this.handleInputChange}
-                    handleFormSubmit={this.handleFormSubmit}
-                />
-                <div className="container">
-                    <h2>Results</h2>
-                    <Results books={this.state.books} />
-                </div>
-            </div>
-        )
-    }
+	render() {
+		console.log(this);
+		return (
+			<div>
+				<Form
+					search={this.state.search}
+					handleInputChange={this.handleInputChange}
+					handleFormSubmit={this.handleFormSubmit}
+				/>
+				<br />
+				<div className="container">
+					
+					{this.state.books.length ? (
+                        <>
+                        <h2>Results</h2>
+						<Results books={this.state.books} />
+                        </>
+					) : (
+						<></>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Search;
